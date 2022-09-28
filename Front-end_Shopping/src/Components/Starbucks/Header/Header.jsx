7fad7be4b89523457d48355c78be4f94d166f31b
Nicolas Castiglioni <atilioncb@gmail.com>
2022-09-27 21:12:39 -0300

    Primer commit front

--- Front-end_Shopping/src/Components/Starbucks/Header/Header.jsx
+++ Front-end_Shopping/src/Components/Starbucks/Header/Header.jsx
@@ -11,11 +11,30 @@ export default function Header() {
       </header>
       <aside>
         <img src={icono} alt={icono} />
-        <h1>Starbucks</h1>{" "}
+        <h1 className="titulo">Starbucks</h1>{" "}
       </aside>
       <article>
-        <section></section>
-        <section></section>
+        <section>
+          {" "}
+          <h2>¡Es temporada de Pumpkin!</h2>
+          <p>
+            Por fin ha llegado el momento más deseado del año,{" "}
+            <strong>
+              nuestro icónico Pumpkin Spice Latte está de vuelta.{" "}
+            </strong>
+            Disfruta de intensos shots de espresso, tu leche favorita y una
+            deliciosa variedad de especias que te harán saborear el otoño.
+            Además, este año puedes probar el PSL en versión Cold Brew, nuestro
+            café macerado durante 20 horas.
+          </p>
+          <button>Encuéntranos </button>
+        </section>
+        <section>
+          <h2>
+            <strong>¡Te lo llevamos!</strong>
+          </h2>
+          <h3>Disfruta tu experiencia Starbucks® donde quieras</h3>
+        </section>
         <section></section>
         <section></section>
       </article>
